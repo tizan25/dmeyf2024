e29baac3d50dfd051fbf825bcb2aa7a54458025f
tizan25 <tizan25@gmail.com>
2024-11-24 16:57:55 -0300

    v6

--- src/workflows/z909_run_orden227.r
+++ src/workflows/z909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/990_workflow_orden227_SEMI_v5.r"
+PARAM <- "src/workflows/990_workflow_orden227_SEMI_v6.r"
 
 envg <- env()
 
