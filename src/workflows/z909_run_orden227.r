7fbc9442db2f053f53f8f7801b7dc99127ff1e1d
tizan25 <tizan25@gmail.com>
2024-11-29 13:41:07 -0300

    semi no undersampling

--- src/workflows/z909_run_orden227.r
+++ src/workflows/z909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/991_workflow_orden228_SEMI_v2.r"
+PARAM <- "src/workflows/991_workflow_orden228_SEMI_v4.r"
 
 envg <- env()
 
