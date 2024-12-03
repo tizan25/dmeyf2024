a97162e731b39131893b279049a09b615c60a68c
tizan25 <tizan25@gmail.com>
2024-12-03 14:00:50 -0300

    semi random forest

--- src/workflows/z909_run_orden227.r
+++ src/workflows/z909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/991_workflow_orden228_SEMI_v5.r"
+PARAM <- "src/workflows/991_workflow_orden228_SEMI_v6.r"
 
 envg <- env()
 
